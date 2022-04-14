cfd6b442bdb4244ad754105ffd9e6604080e0187
gofn92 <gofn92@gmail.com>
2022-04-15 01:34:50 +0900

    change

--- src/apollo.js
+++ src/apollo.js
@@ -1,6 +1,6 @@
 import {ApolloClient, InMemoryCache} from "@apollo/client";
 const client = new ApolloClient({
-  uri: "http://ql-movie-api.herokuapp.com/graphql",
+  uri: "https://ql-movie-api.herokuapp.com/graphql",
   cache: new InMemoryCache(),
 });
 
