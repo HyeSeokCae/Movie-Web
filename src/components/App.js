c7fe3a50f32e1d65f5114f25835954b3047a6742
gofn92 <gofn92@gmail.com>
2022-04-15 21:24:39 +0900

    why...

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ import MovieDetail from "./MovieDetail";
 import ShowDetail from "./ShowDetail";
 function App() {
   return (
-    <Router basename="/movie-ewb">
+    <Router basename="/movie-web">
       <Routes>
         <Route exact path="/" element={<Choice />} />
         <Route path="/phase1" element={<Phase1 />} />
